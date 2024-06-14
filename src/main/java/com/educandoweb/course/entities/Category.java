c1de9e9203ade8daf9ae4cc2987b537a9e4f04ed
Hebert <hebertLandim007@outlook.com>
2024-06-14 10:07:02 -0300

    Product

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -3,7 +3,9 @@ package com.educandoweb.course.entities;
 import jakarta.persistence.*;
 
 import java.io.Serial;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
@@ -16,6 +18,8 @@ public class Category {
     private Long id;
     private String name;
 
+    @Transient
+    private Set<Product> products = new HashSet<>();
     public Category(){
     }
 
@@ -40,6 +44,10 @@ public class Category {
         this.name = name;
     }
 
+    public Set<Product> getProducts() {
+        return products;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -52,4 +60,6 @@ public class Category {
     public int hashCode() {
         return Objects.hash(id);
     }
+
+
 }
